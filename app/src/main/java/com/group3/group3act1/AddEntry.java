7a57b34a82ea8fafcd7e719efe6512eec3bd547f
nekko373 <haveyouseensasha6@gmail.com>
2021-01-18 11:25:14 +0800

    addentry eleements added

---
+++ app/src/main/java/com/group3/group3act1/AddEntry.java
@@ -0,0 +1,14 @@
+package com.group3.group3act1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_entry);
+    }
+}
